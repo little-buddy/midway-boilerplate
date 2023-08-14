73462a2686019665974e17ce97c34bd1abff3cc5
buddy <litoubuddy@gmail.com>
2023-08-15 07:03:41 +0800

    chore: setup dotEnv

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,21 @@
+const dotEnv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 const { Bootstrap } = require('@midwayjs/bootstrap');
+
+// config dotenv
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const pathsDotenv = resolveApp('.env');
+const isEnvProduction = process.env.NODE_ENV === 'production';
+
+dotEnv.config({
+  path: `${pathsDotenv}.${isEnvProduction ? 'production' : 'development'}`,
+});
+
+dotEnv.config({
+  path: pathsDotenv,
+});
+
+/* run -> */
 Bootstrap.run();
